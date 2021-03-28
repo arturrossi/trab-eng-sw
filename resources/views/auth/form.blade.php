ae992779ebb8a5dc0808330e449e28ce167fa21e
Vitor Ferrari <vhdferrari@gmail.com>
2021-03-28 19:50:22 -0300

    corrigindo

--- resources/views/auth/form.blade.php
+++ resources/views/auth/form.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">{{ __('Cadastrar teste') }}</div>
+                <div class="card-header">{{ __('Solicitar teste') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('record.create') }}">
                         @csrf
